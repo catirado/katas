c1c9c62519740956a25c3bde330502cc5540e268
Carlos Tirado <catirado87@gmail.com>
2019-10-20 20:12:04 +0200

    Add FizzBuzz implementation for number 2

---
+++ fizzbuzz/dotnecore/fizzbuzz/dotnecore/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static String Convert(int number)
+        {
+            if (number == 1)
+                return "1";
+            else
+                return "2";
+        }
+    }
+}
