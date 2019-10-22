ffbf3dac8f7dbcf7c9d3b16f8be304cb41b20d67
Carlos Tirado <catirado87@gmail.com>
2019-10-22 21:07:23 +0200

    Add leap year kata

---
+++ leapyear/dotnetcore/LeapYear.Tests/YearShould.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeapYear.Tests
+{
+    public class YearShould
+    {
+
+        [Fact]
+        public void be_a_leap_if_is_divisible_by_400()
+        {
+            var year = new Year(2000);
+            year.IsLeap().Should().BeTrue();  
+        }
+
+        [Fact]
+        public void not_be_a_leap_if_is_divisible_by_100_and_not_by_400()
+        {
+            var year = new Year(1800);
+            year.IsLeap().Should().BeFalse();
+        }
+
+        [Fact]
+        public void be_a_leap_if_is_divisible_by_4()
+        {
+            var year = new Year(2016);
+            year.IsLeap().Should().BeTrue();
+        }
+
+        [Fact]
+        public void not_be_a_leap_if_is_not_divisible_by_4()
+        {
+            var year = new Year(2017);
+            year.IsLeap().Should().BeFalse();
+        }
+
+
+    }
+}
---
+++ leapyear/dotnetcore/LeapYear/Year.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeapYear
+{
+    public class Year
+    {
+        private int year;
+
+        public Year(int year)
+        {
+            this.year = year;
+        }
+
+        public bool IsLeap()
+        {
+            if (isDivisibleBy(400))
+                return true;
+
+            if (isDivisibleBy(100))
+                return false;
+
+            return isDivisibleBy(4);
+
+            bool isDivisibleBy(int factor) => year % factor == 0;
+        }            
+        
+    }
+}
