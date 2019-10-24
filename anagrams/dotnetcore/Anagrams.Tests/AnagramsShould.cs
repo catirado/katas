a45ef88eea097f31508f24fc304b94661140cfe0
Carlos Tirado <catirado87@gmail.com>
2019-10-24 21:01:44 +0200

    Add anagrams kata

---
+++ anagrams/dotnetcore/Anagrams.Tests/AnagramsShould.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace Anagrams.Tests
+{
+    public class AnagramsShould
+    {
+        [Fact]
+        public void return_empty_list_if_the_input_is_empty()
+        {
+            Anagrams.Of("").Should().BeEmpty();
+        }
+
+        [Fact]
+        public void return_the_list_with_himself_if_the_input_has_one_character()
+        {
+            Anagrams.Of("a").Should().BeEquivalentTo(new List<String> { "a" });
+        }
+
+        [Fact]
+        public void return_the_list_of_anagrams_if_the_input_has_two_character()
+        {
+            var expectedAnagrams = new List<String> { "bi", "ib" };
+            Anagrams.Of("bi").Should().BeEquivalentTo(expectedAnagrams);
+        }
+
+        [Fact]
+        public void return_the_list_of_anagrams_if_the_input_has_three_characters()
+        {
+            var expectedAnagrams = new List<String> { "bir", "bri", "ibr", "irb", "rbi", "rib" };
+            Anagrams.Of("bir").Should().BeEquivalentTo(expectedAnagrams);
+        }
+
+        [Fact]
+        public void return_the_list_of_anagrams_if_the_input_has_four_characters()
+        {
+            var expectedAnagrams = new List<String> { "biro", "bior", "brio", "broi", "boir", "bori",
+                                                      "ibro", "ibor", "irbo", "irob", "iobr", "iorb",
+                                                      "rbio", "rboi", "ribo", "riob", "roib", "robi",
+                                                      "obir", "obri", "oibr", "oirb", "orbi", "orib"};
+            Anagrams.Of("biro").Should().BeEquivalentTo(expectedAnagrams);
+        }
+    }
+}
---
+++ anagrams/dotnetcore/Anagrams/Anagrams.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Anagrams
+{
+    public class Anagrams
+    {
+        public static IList<String> Of(string input)
+        {
+            var anagrams = new List<String>();
+
+            if (input.Length == 1)
+                return new List<String> { input };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                String character = input[i].ToString();
+                var anagramsOfRestLetters = Anagrams.Of(RemoveCharFromPosition(input, i));
+                var anagramasWithCharacterFirst = anagramsOfRestLetters.Select(restLettersAnagrams => character + restLettersAnagrams);
+                anagrams.AddRange(anagramasWithCharacterFirst);
+            }
+
+            return anagrams;
+        }
+
+        private static string RemoveCharFromPosition(string input, int index)
+        {
+            return input.Remove(index, 1);
+        }
+    }
+}
