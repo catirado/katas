8bdc643c602d4349694f6fddc38d0f1fdab855fe
Carlos Tirado <catirado87@gmail.com>
2020-04-19 13:46:25 +0200

    Add gilded rose kata with golden master tests

---
+++ gildedrose/dotnetcore/GildedRose.Tests/GildedRoseTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace GildedRose.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void updateQuantity()
+        {
+            var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            string itemString = items[0].Name.ToString();
+            Approvals.Verify(itemString);
+        }
+    }
+}
