297ab9a7317e064cc4dd39bc41dfa56ea70607f5
Carlos Tirado <catirado87@gmail.com>
2020-04-19 21:33:46 +0200

    Remove not used code

--- gildedrose/dotnetcore/GildedRose/GildedRose.cs
+++ gildedrose/dotnetcore/GildedRose/GildedRose.cs
@@ -28,36 +28,13 @@ namespace GildedRose
                     item.Quality = item.Quality + 1;
                 }
 
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
+                item.SellIn = item.SellIn - 1;
 
                 if (item.SellIn < 0)
                 {
-                    if (item.Name != "Aged Brie")
+                    if (item.Quality < 50)
                     {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
