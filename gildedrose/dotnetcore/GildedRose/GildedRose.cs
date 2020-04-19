331a3fcea4809c875fa2b4d7c6495266db99351a
Carlos Tirado <catirado87@gmail.com>
2020-04-19 21:43:08 +0200

    Remove unreachable code

--- gildedrose/dotnetcore/GildedRose/GildedRose.cs
+++ gildedrose/dotnetcore/GildedRose/GildedRose.cs
@@ -81,29 +81,19 @@ namespace GildedRose
 
                 if (item.SellIn < 0)
                 {
-                    if (item.Name != "Aged Brie")
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Quality > 0)
                         {
-                            if (item.Quality > 0)
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
+                                item.Quality = item.Quality - 1;
                             }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
                         }
                     }
                     else
                     {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
+                        item.Quality = item.Quality - item.Quality;
                     }
                 }
             }
