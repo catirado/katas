927523393e7627af5470583b5b29196f91aa579a
Carlos Tirado <catirado87@gmail.com>
2020-04-24 14:35:38 +0200

    Add new gilded rose session

---
+++ gildedrose/dotnetcore-mob/GildedRose.Tests/GildedRoseTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using ApprovalTests;
+using ApprovalTests.Combinations;
+using ApprovalTests.Reporters;
+
+namespace GildedRose.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class gilded_rose_should
+    {
+        [Fact]
+        public void update_quality()
+        {
+            var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            string itemString = items[0].Name;
+            Approvals.Verify(itemString);
+        }
+    }
+}
