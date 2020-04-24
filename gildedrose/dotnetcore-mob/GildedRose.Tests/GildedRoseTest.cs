5c59a5ad7b63814370e830b7a295840bd6fb7899
Carlos Tirado <catirado87@gmail.com>
2020-04-24 17:32:41 +0200

    Add coverga

--- gildedrose/dotnetcore-mob/GildedRose.Tests/GildedRoseTest.cs
+++ gildedrose/dotnetcore-mob/GildedRose.Tests/GildedRoseTest.cs
@@ -12,11 +12,24 @@ namespace GildedRose.Tests
         [Fact]
         public void update_quality()
         {
-            var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            var name = new List<string>() {"foo", "Aged Brie","Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "Conjured Mana Cake" };
+            var sellIn = new List<int>() {-1, 0, 5, 6, 7, 10, 11, 12};
+            var quality = new List<int>() {-1, 0, 1, 2, 49, 50, 51};
+            
+            CombinationApprovals.VerifyAllCombinations(
+                ExecuteUpdateQuality,
+                name,
+                sellIn,
+                quality);
+        }
+        
+        private string ExecuteUpdateQuality(string name, int sellIn, int quality)
+        {
+            var items = new List<Item> {new Item {Name = name, SellIn = sellIn, Quality = quality}};
             var app = new GildedRose(items);
             app.UpdateQuality();
-            string itemString = items[0].Name;
-            Approvals.Verify(itemString);
+            return items[0].ToString();
         }
+        
     }
 }
